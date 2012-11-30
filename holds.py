0c40a61aa7656b7c4b4918bb275a48db6a325894
Egor Tsiaulouski <et324@mit.edu>
2012-11-30 15:45:14 -0500

    added damping controller

--- holds.py
+++ holds.py
@@ -45,6 +45,35 @@ class SimpleHoldController(HoldController):
         else:
             return False
 
+        
+class DampingController(HoldController):
+    """Simple illustration of a controller class"""
+    def __init__(self, theta_1, theta_2, theta_3):
+        """theta_1, theta_1, theta_3 are the target angles for the motors"""
+        self.theta_1 = theta_1
+        self.theta_2 = theta_2
+        self.theta_3 = theta_3
+        
+    def control(self, q, q_dot):
+        return generate_command_map(
+            generate_servo_command(self.theta_1, torque_percentage = 0.7),
+            generate_servo_command(self.theta_2, torque_percentage = 0.7),
+            generate_servo_command(self.theta_3, torque_percentage = 0.7))
+        
+    def is_stable(self, q, q_dot):
+        eps = 0.01
+        
+        # make sure all of the generalized coordinates are no longer moving
+        for q_i_dot in q_dot:
+            if abs(q_i_dot) > eps:
+                return False
+
+        # if we are not moving, make sure we are at the target position
+        if abs(q[0] - self.theta_1) < eps and abs(q[1] - self.theta_2) < eps and abs(q[2] - self.theta_3) < eps:
+            return True
+        else:
+            return False
+
 ### Hold instances below here
 DEAD_HANG = Hold("Dead Hang", SimpleHoldController(0, 0, -math.pi/2))
 
