f5b314f62c4388227a46bd9d638735dd1b708e18
WAT7SY <tom.e.watts@btconnect.com>
2012-12-02 22:17:54 -0500

    Added checking for stable values

--- arduino_config.py
+++ arduino_config.py
@@ -20,13 +20,18 @@ def get_IMU_data():
     return IMU_data
 def IMU_zero():
     GYRO_settled=False
-    GYROY=get_IMU_data()[4]
-    while GYROY > 30:
-        GYROY=get_IMU_data()[4]
+    init_data=None
+    while not GYRO_settled
+        GYRO_settled=True
+        init_data=get_IMU_data()
+        for gyro in init_data[3:]:
+           if abs(gyro) > 5
+               GYRO_settled = False
+               print UNSTABLE
+
+    return init_data
     
-    ser.write('\1')
-    IMU_data_zero_raw = ser.readline();
-    IMU_data_zero = map(float, IMU_data_zero_raw.strip().split("\t"))
+   
     
     return IMU_data_zero
 def initialise_serial():
@@ -50,12 +55,7 @@ def initialise_serial():
     GYROX0=IMU_VALS[3]
     GYROY0=IMU_VALS[4]
     GYROZ0=IMU_VALS[5]
-    return YAW0
-    return PITCH0
-    return ROLL0
-    return GYROX0
-    return GYROY0
-    return GYROZ0
+    
 
 
 initialise_serial()
